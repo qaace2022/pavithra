403c921ccf5a4f5d80d8d0ef24fcabcf7b7f4606
pavithra077 <kumarpavithra757@gmail.com>
2022-02-15 17:54:16 +0530

    Day2-Exercise
    Exercise 2 Answers

---
+++ FirstApp/src/qaace22training/ConverDoWhileToWhile.java
@@ -0,0 +1,17 @@
+package qaace22training;
+
+public class ConverDoWhileToWhile {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i=1;
+		while(i<6)
+		{
+			System.out.println("I will get printed once for sure");
+			i++;
+		}
+		
+
+	}
+
+}
---
+++ FirstApp/src/qaace22training/LeftPyramid.java
@@ -0,0 +1,23 @@
+package qaace22training;
+
+public class LeftPyramid {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i, j, n = 5;
+		for (i=0; i<n; i++)
+		{
+		  for (j=2*(n-i); j>=0; j--)
+		  {
+		    System.out.print(" ");
+		  }
+		  for (j=0; j<=i; j++)
+		  {
+		    System.out.print("* ");
+		  }
+		  System.out.println();
+		}
+
+	}
+
+}
